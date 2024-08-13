0c22683bb973bd585af7f20a719de55409035633
keyne <kleuyckx@gmail.com>
2024-08-13 14:36:55 +0100

    create authorization structure

--- contracts/authorization/src/state.rs
+++ contracts/authorization/src/state.rs
@@ -1,4 +1,7 @@
+use authorization_utils::authorization::Authorization;
 use cosmwasm_std::{Addr, Empty};
 use cw_storage_plus::Map;
 
 pub const SUB_OWNERS: Map<Addr, Empty> = Map::new("sub_owners");
+
+pub const AUTHORIZATIONS: Map<String, Authorization> = Map::new("authorizations");
