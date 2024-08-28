6ec6723448ddf41f14ddef586fbee6e43c10f05c
Art3miX <40179351+Art3miX@users.noreply.github.com>
2024-08-28 13:17:48 +0300

    fix issue #11 (#13)

--- contracts/accounts/base_account/src/state.rs
+++ contracts/accounts/base_account/src/state.rs
@@ -1,5 +1,5 @@
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Empty};
 use cw_storage_plus::{Item, Map};
 
 pub const ADMIN: Item<Addr> = Item::new("admin");
-pub const APPROVED_SERVICES: Map<Addr, bool> = Map::new("services");
+pub const APPROVED_SERVICES: Map<Addr, Empty> = Map::new("services");
