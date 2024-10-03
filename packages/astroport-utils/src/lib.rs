3bd623a6145bd047139e01388168e11b0d48ec46
Keyne <kleuyckx@gmail.com>
2024-10-03 10:22:45 +0100

    Astroport LPer combined (#40)
    * initial structure
    
    * first version
    
    * refactor code
    
    * remove service base because of incompatibility with astroport deps
    
    * remove unused
    
    * only processor start actions
    
    * check if pool type and pool assets match in same check
    
    * separate mod in different files
    
    * add rest of traits of service-base
    
    * add pre-validate
    
    * test suite for Astroport
    
    * mint to input account to start testing
    
    * add more tests
    
    * add pool type check
    
    * refactor and add LPerTestSuite
    
    * fix file name in tests
    
    * final tests
    
    * add comments
    
    * refactor
    
    * reuse execute_on_behalf_of
    
    * fix read me
    
    * change pairinfo for cw20
    
    * add comment to specify astroport package version used
    
    * fix comment
    
    * move decimalcheckedops to astroport utils
    
    * use into

---
+++ packages/astroport-utils/src/lib.rs
@@ -0,0 +1,31 @@
+pub mod astroport_cw20_lp_token;
+pub mod astroport_native_lp_token;
+#[cfg(feature = "testing")]
+pub mod suite;
+
+// Implemented in the astroport crate for Decimal
+pub mod decimal_checked_ops {
+    use std::convert::TryInto;
+
+    use cosmwasm_std::{Decimal, Fraction, OverflowError, Uint128, Uint256};
+
+    // We define the helper for Decimals that is defined in the astroport crate
+    pub trait DecimalCheckedOps {
+        fn checked_mul_uint128(self, other: Uint128) -> Result<Uint128, OverflowError>;
+    }
+
+    impl DecimalCheckedOps for Decimal {
+        fn checked_mul_uint128(self, other: Uint128) -> Result<Uint128, OverflowError> {
+            if self.is_zero() || other.is_zero() {
+                return Ok(Uint128::zero());
+            }
+            let multiply_ratio =
+                other.full_mul(self.numerator()) / Uint256::from(self.denominator());
+            if multiply_ratio > Uint256::from(Uint128::MAX) {
+                Err(OverflowError::new(cosmwasm_std::OverflowOperation::Mul))
+            } else {
+                Ok(multiply_ratio.try_into().unwrap())
+            }
+        }
+    }
+}
