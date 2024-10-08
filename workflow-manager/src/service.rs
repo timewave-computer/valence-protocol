c3822f2e639e123f2bb094ab9d1cc8f1962c3ec6
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2024-10-08 13:44:43 +0200

    Reverse Splitter service (#49)
    * Implement splitter service - WIP
    
    * Implement pre_validate func on ServiceConfig
    
    * Implement calculations for ratios
    
    * Fix broken test
    
    * Add test for a mix of 3 tokens incl. a CW20 and ratios
    
    * cargo fmt
    
    * Add README
    
    * Remove splitter-temp service
    
    * Add support for config update + tests
    
    * Added test; fix build
    
    * Reverse splitter service
    
    * Add support for dynamic ratio
    
    * Use ServiceAccountType for account addresses in ServiceConfig
    
    * Address PR review feedback; add zero amount & ratio checks
    
    * Simplify Reverse Splitter service config
    
    * Address PR review feedback

--- workflow-manager/src/service.rs
+++ workflow-manager/src/service.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::to_vec;
 use strum::VariantNames;
 use thiserror::Error;
-use valence_reverse_splitter::msg::ServiceConfig as ReverseSplitterServiceConfig;
+use valence_reverse_splitter_service::msg::ServiceConfig as ReverseSplitterServiceConfig;
 use valence_service_utils::{msg::InstantiateMsg, Id, ServiceConfigInterface};
 use valence_splitter_service::msg::ServiceConfig as SplitterServiceConfig;
 
