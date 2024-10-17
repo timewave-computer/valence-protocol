b0e4dfae837df03a4d200c1ad3ef9e7349660235
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2024-10-17 18:23:49 +0200

    Revert "Add support for PFM in Generic IBC transfer service"
    This reverts commit a958a04220a9711bcabd55b2511547d1aed23c17.

--- packages/ibc-utils/src/generic.rs
+++ packages/ibc-utils/src/generic.rs
@@ -1,10 +1,8 @@
 use std::collections::BTreeMap;
 
-use cosmwasm_std::{
-    coin, to_json_string, CosmosMsg, Env, IbcTimeout, IbcTimeoutBlock, StdResult, Timestamp,
-};
+use cosmwasm_std::{coin, CosmosMsg, Env, IbcTimeout, IbcTimeoutBlock, StdResult, Timestamp};
 
-use crate::types::{ForwardMetadata, PacketForwardMiddlewareConfig, PacketMetadata};
+use crate::types::PacketForwardMiddlewareConfig;
 
 // Default timeout for IbcTransfer is 600 seconds
 const DEFAULT_TIMEOUT_TIMESTAMP: u64 = 600;
@@ -19,53 +17,27 @@ pub fn ibc_send_message(
     memo: String,
     timeout_height: Option<u64>,
     timeout_timestamp: Option<u64>,
-    denom_to_pfm_map: BTreeMap<String, PacketForwardMiddlewareConfig>,
+    _denom_to_pfm_map: BTreeMap<String, PacketForwardMiddlewareConfig>,
 ) -> StdResult<CosmosMsg> {
-    let msg = match denom_to_pfm_map.get(&denom) {
-        None => cosmwasm_std::IbcMsg::Transfer {
-            channel_id: channel,
-            to_address: to,
-            amount: coin(amount, denom),
-            timeout: get_timeout(&env, timeout_height, timeout_timestamp),
-            memo: Some(memo),
-        },
-        Some(pfm_config) => cosmwasm_std::IbcMsg::Transfer {
-            channel_id: pfm_config.local_to_hop_chain_channel_id.to_string(),
-            to_address: pfm_config.hop_chain_receiver_address.to_string(),
-            amount: coin(amount, denom),
-            timeout: get_timeout(&env, timeout_height, timeout_timestamp),
-            memo: Some(to_json_string(&PacketMetadata {
-                forward: Some(ForwardMetadata {
-                    receiver: to.clone(),
-                    port: "transfer".to_string(),
-                    // hop chain to final receiver chain channel
-                    channel: pfm_config.hop_to_destination_chain_channel_id.to_string(),
-                }),
-            })?),
-        },
-    };
-
-    Ok(CosmosMsg::Ibc(msg))
-}
-
-fn get_timeout(
-    env: &Env,
-    timeout_height: Option<u64>,
-    timeout_timestamp: Option<u64>,
-) -> IbcTimeout {
-    match (timeout_height, timeout_timestamp) {
-        (Some(height), None) => IbcTimeout::with_block(IbcTimeoutBlock {
-            revision: 2,
-            height,
-        }),
-        (None, Some(timestamp)) => IbcTimeout::with_timestamp(Timestamp::from_nanos(timestamp)),
-        (Some(height), Some(timestamp)) => IbcTimeout::with_both(
-            IbcTimeoutBlock {
+    Ok(CosmosMsg::Ibc(cosmwasm_std::IbcMsg::Transfer {
+        channel_id: channel,
+        to_address: to,
+        amount: coin(amount, denom),
+        timeout: match (timeout_height, timeout_timestamp) {
+            (Some(height), None) => IbcTimeout::with_block(IbcTimeoutBlock {
                 revision: 2,
                 height,
-            },
-            Timestamp::from_nanos(timestamp),
-        ),
-        _ => IbcTimeout::with_timestamp(env.block.time.plus_seconds(DEFAULT_TIMEOUT_TIMESTAMP)),
-    }
+            }),
+            (None, Some(timestamp)) => IbcTimeout::with_timestamp(Timestamp::from_nanos(timestamp)),
+            (Some(height), Some(timestamp)) => IbcTimeout::with_both(
+                IbcTimeoutBlock {
+                    revision: 2,
+                    height,
+                },
+                Timestamp::from_nanos(timestamp),
+            ),
+            _ => IbcTimeout::with_timestamp(env.block.time.plus_seconds(DEFAULT_TIMEOUT_TIMESTAMP)),
+        },
+        memo: Some(memo),
+    }))
 }
