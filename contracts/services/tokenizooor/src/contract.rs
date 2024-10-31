481265ff7ae2a4d139b9633c0abf3532bd97e3f4
bekauz <bekauz@protonmail.com>
2024-10-31 07:15:21 +0100

    tokenize method

--- contracts/services/tokenizooor/src/contract.rs
+++ contracts/services/tokenizooor/src/contract.rs
@@ -1,10 +1,10 @@
-use cosmos_sdk_proto::traits::MessageExt;
+use cosmos_sdk_proto::{cosmos::base::v1beta1::Coin, traits::MessageExt};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
     to_json_binary, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
 };
-use neutron_sdk::proto_types::osmosis::tokenfactory::v1beta1::MsgCreateDenom;
+use neutron_sdk::proto_types::osmosis::tokenfactory::v1beta1::{MsgCreateDenom, MsgMint};
 use valence_service_utils::{
     error::ServiceError,
     msg::{ExecuteMsg, InstantiateMsg},
@@ -58,6 +58,23 @@ fn create_denom_msg(sender: String) -> CosmosMsg {
     }
 }
 
+fn mint_msg(sender: String, recipient: String, amount: u128, denom: String) -> CosmosMsg {
+    let msg_mint = MsgMint {
+        sender,
+        amount: Some(Coin {
+            denom,
+            amount: amount.to_string(),
+        }),
+        mint_to_address: recipient,
+    };
+    // TODO: Change to AnyMsg instead of Stargate when we can test with CW 2.0 (They are the same, just a rename)
+    #[allow(deprecated)]
+    CosmosMsg::Stargate {
+        type_url: "/osmosis.tokenfactory.v1beta1.MsgMint".to_string(),
+        value: Binary::from(msg_mint.to_bytes().unwrap()),
+    }
+}
+
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
@@ -76,31 +93,93 @@ pub fn execute(
 }
 
 mod actions {
-    use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
-    use valence_service_utils::error::ServiceError;
+    use std::collections::BTreeMap;
 
-    use crate::msg::{ActionMsgs, Config};
+    use cosmwasm_std::{
+        coin, ensure, Addr, BankMsg, DepsMut, Env, MessageInfo, Response, StdError, Uint128,
+    };
+    use valence_service_utils::{error::ServiceError, execute_on_behalf_of};
+
+    use crate::{
+        msg::{ActionMsgs, Config},
+        state::TOKENIZED_DENOM,
+    };
+
+    use super::mint_msg;
 
     pub fn process_action(
         deps: DepsMut,
         env: Env,
-        info: MessageInfo,
+        _info: MessageInfo,
         msg: ActionMsgs,
         cfg: Config,
     ) -> Result<Response, ServiceError> {
         match msg {
-            ActionMsgs::Tokenize { sender } => try_tokenize(deps, env, info, sender, cfg),
+            ActionMsgs::Tokenize { sender } => try_tokenize(deps, env, sender, cfg),
         }
     }
 
     fn try_tokenize(
-        _deps: DepsMut,
-        _env: Env,
-        _info: MessageInfo,
+        deps: DepsMut,
+        env: Env,
         sender: String,
-        _cfg: Config,
+        cfg: Config,
     ) -> Result<Response, ServiceError> {
-        Ok(Response::new().add_attribute("method", "noop"))
+        // build a map of sender balances relevant to tokenizing the share.
+        // denom -> balance
+        let mut sender_balances_map: BTreeMap<String, Uint128> = BTreeMap::new();
+        for (denom, _) in cfg.input_denoms.iter() {
+            let sender_balance = deps.querier.query_balance(sender.to_string(), denom)?;
+            sender_balances_map.insert(sender_balance.denom, sender_balance.amount);
+        }
+
+        let mut output_shares = Uint128::zero();
+        for (denom, sender_balance) in sender_balances_map.iter() {
+            // we check what amount of this denom is needed to buy a share
+            let price = cfg.input_denoms.get(denom).unwrap();
+            // then we calculate how many shares the sender can buy with this denom amount
+            let share_amount = sender_balance.checked_div(*price).unwrap();
+
+            // if output shares is uninitialized, set it to the share amount
+            // otherwise validate that currently observed denom meets the requirement
+            if output_shares.is_zero() {
+                output_shares = share_amount;
+            } else {
+                ensure!(
+                    share_amount == output_shares,
+                    StdError::generic_err("Invalid share amount")
+                );
+            }
+        }
+
+        let mut price_tokens = vec![];
+        for (denom, price) in cfg.input_denoms.iter() {
+            let amount_to_send = price.checked_mul(output_shares).unwrap();
+            price_tokens.push(coin(amount_to_send.u128(), denom));
+        }
+
+        let transfer_msg = BankMsg::Send {
+            to_address: cfg.output_addr.to_string(),
+            amount: price_tokens,
+        };
+
+        let tf_denom = TOKENIZED_DENOM.load(deps.storage)?;
+
+        let mint_msg = mint_msg(
+            env.contract.address.to_string(),
+            sender.to_string(),
+            output_shares.u128(),
+            tf_denom,
+        );
+
+        let delegated_msgs = execute_on_behalf_of(
+            vec![transfer_msg.into(), mint_msg],
+            &Addr::unchecked(sender),
+        )?;
+
+        Ok(Response::new()
+            .add_attribute("method", "tokenize")
+            .add_message(delegated_msgs))
     }
 }
 
--- contracts/services/tokenizooor/src/msg.rs
+++ contracts/services/tokenizooor/src/msg.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Addr, Decimal, Deps, DepsMut};
+use cosmwasm_std::{Addr, Deps, DepsMut, Uint128};
 use cw_ownable::cw_ownable_query;
 use valence_macros::{valence_service_query, ValenceServiceInterface};
 use valence_service_utils::ServiceAccountType;
@@ -15,15 +15,15 @@ pub enum ActionMsgs {
 #[cw_serde]
 pub struct Config {
     pub output_addr: Addr,
-    // map of denom to input ratio, e.g.
+    // map of denoms to input amount, e.g.
     // { "atom": 1, "usdc": 10 } would mean that
     // each tokenized output position would require
     // 1 atom and 10 usdc
-    pub input_denoms: BTreeMap<String, Decimal>,
+    pub input_denoms: BTreeMap<String, Uint128>,
 }
 
 impl Config {
-    pub fn new(output_addr: Addr, input_denoms: BTreeMap<String, Decimal>) -> Self {
+    pub fn new(output_addr: Addr, input_denoms: BTreeMap<String, Uint128>) -> Self {
         Config {
             output_addr,
             input_denoms,
@@ -42,14 +42,14 @@ pub enum QueryMsg {}
 pub struct ServiceConfig {
     pub input_addr: ServiceAccountType,
     pub output_addr: ServiceAccountType,
-    pub input_denoms: BTreeMap<String, Decimal>,
+    pub input_denoms: BTreeMap<String, Uint128>,
 }
 
 impl ServiceConfig {
     pub fn new(
         input_addr: impl Into<ServiceAccountType>,
         output_addr: impl Into<ServiceAccountType>,
-        input_denoms: BTreeMap<String, Decimal>,
+        input_denoms: BTreeMap<String, Uint128>,
     ) -> Self {
         ServiceConfig {
             input_addr: input_addr.into(),
@@ -61,7 +61,7 @@ impl ServiceConfig {
     fn do_validate(
         &self,
         api: &dyn cosmwasm_std::Api,
-    ) -> Result<(Addr, BTreeMap<String, Decimal>), ServiceError> {
+    ) -> Result<(Addr, BTreeMap<String, Uint128>), ServiceError> {
         let output_addr = self.input_addr.to_addr(api)?;
 
         Ok((output_addr, self.input_denoms.clone()))
