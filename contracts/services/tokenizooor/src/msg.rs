76d6b11ca08a3ba9241b365365638b1e7f6fd7b6
bekauz <bekauz@protonmail.com>
2024-10-31 05:40:25 +0100

    tokenizer msg

--- contracts/services/tokenizooor/src/msg.rs
+++ contracts/services/tokenizooor/src/msg.rs
@@ -1,5 +1,7 @@
+use std::collections::BTreeMap;
+
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Addr, Deps, DepsMut};
+use cosmwasm_std::{Addr, Decimal, Deps, DepsMut};
 use cw_ownable::cw_ownable_query;
 use valence_macros::{valence_service_query, ValenceServiceInterface};
 use valence_service_utils::ServiceAccountType;
@@ -13,11 +15,25 @@ pub enum ActionMsgs {
 #[cw_serde]
 pub struct Config {
     pub input_addr: Addr,
+    pub output_addr: Addr,
+    // map of denom to input ratio, e.g.
+    // { "atom": 1, "usdc": 10 } would mean that
+    // each tokenized output position would require
+    // 1 atom and 10 usdc
+    pub input_denoms: BTreeMap<String, Decimal>,
 }
 
 impl Config {
-    pub fn new(input_addr: Addr) -> Self {
-        Config { input_addr }
+    pub fn new(
+        input_addr: Addr,
+        output_addr: Addr,
+        input_denoms: BTreeMap<String, Decimal>,
+    ) -> Self {
+        Config {
+            input_addr,
+            output_addr,
+            input_denoms,
+        }
     }
 }
 
@@ -31,18 +47,31 @@ pub enum QueryMsg {}
 #[derive(ValenceServiceInterface)]
 pub struct ServiceConfig {
     pub input_addr: ServiceAccountType,
+    pub output_addr: ServiceAccountType,
+    pub input_denoms: BTreeMap<String, Decimal>,
 }
 
 impl ServiceConfig {
-    pub fn new(input_addr: impl Into<ServiceAccountType>) -> Self {
+    pub fn new(
+        input_addr: impl Into<ServiceAccountType>,
+        output_addr: impl Into<ServiceAccountType>,
+        input_denoms: BTreeMap<String, Decimal>,
+    ) -> Self {
         ServiceConfig {
             input_addr: input_addr.into(),
+            output_addr: output_addr.into(),
+            input_denoms,
         }
     }
 
-    fn do_validate(&self, api: &dyn cosmwasm_std::Api) -> Result<Addr, ServiceError> {
+    fn do_validate(
+        &self,
+        api: &dyn cosmwasm_std::Api,
+    ) -> Result<(Addr, Addr, BTreeMap<String, Decimal>), ServiceError> {
         let input_addr = self.input_addr.to_addr(api)?;
-        Ok(input_addr)
+        let output_addr = self.input_addr.to_addr(api)?;
+
+        Ok((input_addr, output_addr, self.input_denoms.clone()))
     }
 }
 
@@ -54,9 +83,13 @@ impl ServiceConfigValidation<Config> for ServiceConfig {
     }
 
     fn validate(&self, deps: Deps) -> Result<Config, ServiceError> {
-        let input_addr = self.do_validate(deps.api)?;
+        let (input_addr, output_addr, map) = self.do_validate(deps.api)?;
 
-        Ok(Config { input_addr })
+        Ok(Config {
+            input_addr,
+            output_addr,
+            input_denoms: map,
+        })
     }
 }
 
