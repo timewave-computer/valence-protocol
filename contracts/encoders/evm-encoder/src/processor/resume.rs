3a287d6a933cf8ed190ab0c196b671073cab90e8
Keyne <kleuyckx@gmail.com>
2024-12-23 10:43:24 +0000

    Encoder broker, EVM encoder and solidity decoding refactor (#125)

---
+++ contracts/encoders/evm-encoder/src/processor/resume.rs
@@ -0,0 +1,14 @@
+use alloy_primitives::Bytes;
+use alloy_sol_types::SolValue;
+use cosmwasm_std::Binary;
+use valence_encoder_utils::processor::solidity_types::{ProcessorMessage, ProcessorMessageType};
+
+pub fn encode() -> Binary {
+    let processor_message = ProcessorMessage {
+        messageType: ProcessorMessageType::Resume,
+        // No data is required for the Resume message
+        message: Bytes::new(),
+    };
+
+    Binary::new(processor_message.abi_encode())
+}
