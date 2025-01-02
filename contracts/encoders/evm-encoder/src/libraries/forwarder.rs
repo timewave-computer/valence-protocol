cb85e1a69f131b7de0f48af7e8f9e6f32f4dfa29
keyne <kleuyckx@gmail.com>
2025-01-02 15:34:47 +0000

    forwarder encoding part 1

--- contracts/encoders/evm-encoder/src/libraries/forwarder.rs
+++ contracts/encoders/evm-encoder/src/libraries/forwarder.rs
@@ -1,22 +1,42 @@
-use alloy_sol_types::{sol, SolCall};
-use cosmwasm_std::{Binary, StdError, StdResult};
-use valence_forwarder_library::msg::{Config, FunctionMsgs};
-use valence_library_utils::msg::ExecuteMsg;
+use std::str::FromStr;
 
-// Define our forwarder library API
-// TODO: Add the updateConfig, updateProcessor and updateOwnership once we get to the library implementation
-sol! {
-    function forward() external view;
+use alloy_primitives::Address;
+use alloy_sol_types::SolCall;
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Binary, StdError, StdResult};
+use valence_encoder_utils::libraries::forwarder::solidity_types::forwardCall;
+use valence_forwarder_library::msg::{ForwardingConstraints, FunctionMsgs};
+use valence_library_utils::{msg::ExecuteMsg, LibraryAccountType};
+
+// We need to define a new config that will be used to encode the message because the one from the CW library is not the same as the one from the Solidity library
+#[cw_serde]
+/// Struct representing the library configuration.
+pub struct LibraryConfig {
+    /// The input address for the library.
+    pub input_addr: LibraryAccountType,
+    /// The output address for the library.
+    pub output_addr: LibraryAccountType,
+    /// The forwarding configurations for the library.
+    pub forwarding_configs: Vec<ForwardingConfig>,
+    /// The forwarding constraints for the library.
+    pub forwarding_constraints: ForwardingConstraints,
 }
+
+#[cw_serde]
+pub struct ForwardingConfig {
+    /// The address of the token to forward.
+    pub token_address: String,
+    /// The maximum amount to forward.
+    pub max_amount: u128,
+}
+
+type ForwarderMsg = ExecuteMsg<FunctionMsgs, LibraryConfig>;
 
 pub fn encode(msg: &Binary) -> StdResult<Vec<u8>> {
     // Extract the message from the binary and verify that it parses into a valid json for the library
-    let msg: ExecuteMsg<FunctionMsgs, Config> =
-        serde_json::from_slice(msg.as_slice()).map_err(|_| {
-            StdError::generic_err(
-                "Message sent is not a valid message for this library!".to_string(),
-            )
-        })?;
+    let msg: ForwarderMsg = serde_json::from_slice(msg.as_slice()).map_err(|_| {
+        StdError::generic_err("Message sent is not a valid message for this library!".to_string())
+    })?;
 
     match msg {
         ExecuteMsg::ProcessFunction(function) => {
@@ -29,8 +49,37 @@ pub fn encode(msg: &Binary) -> StdResult<Vec<u8>> {
                 }
             }
         }
-        // TODO: Decide what is going to be allowed to be updated from here in the library
-        ExecuteMsg::UpdateConfig { .. } => todo!(),
+        ExecuteMsg::UpdateConfig { new_config } => {
+            let input_account = Address::from_str(&new_config.input_addr.to_string()?).map_err(|e| {
+                StdError::generic_err(format!("Error parsing input address: {}", e))
+            })?;
+
+            let output_account = Address::from_str(&new_config.output_addr.to_string()?).map_err(|e| {
+                StdError::generic_err(format!("Error parsing output address: {}", e))
+            })?;
+
+            let forwarding_configs=
+
+
+            let forwarder_config =
+                valence_encoder_utils::libraries::forwarder::solidity_types::ForwarderConfig {
+                    inputAccount: input_account,
+                    outputAccount: output_account,
+                    forwarding_configs: new_config
+                        .forwarding_configs
+                        .iter()
+                        .map(|fwd_cfg| {
+                            Ok(valence_encoder_utils::libraries::forwarder::solidity_types::ForwardingConfig {
+                                tokenAddress: Address::from_str(&fwd_cfg.token_address)
+                                    .map_err(|e| StdError::generic_err(format!("Error parsing token address: {}", e)))?,
+                                maxAmount: fwd_cfg.max_amount,
+                            })
+                        })
+                        .collect::<Result<Vec<_>, _>>()?
+                    intervalType: ,
+                    minInterval: todo!(),
+                };
+        }
         ExecuteMsg::UpdateProcessor { .. } => todo!(),
         ExecuteMsg::UpdateOwnership(..) => todo!(),
     }
