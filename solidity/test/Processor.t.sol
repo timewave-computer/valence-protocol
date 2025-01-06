c74c6fade2f43913d80d9ef3170165a83d3717fa
Keyne <kleuyckx@gmail.com>
2025-01-06 13:57:46 +0000

    EVM LiteProcessor (#133)

--- solidity/test/Processor.t.sol
+++ solidity/test/Processor.t.sol
@@ -3,24 +3,37 @@ pragma solidity ^0.8.28;
 
 import {Test, console} from "forge-std/src/Test.sol";
 import {Processor} from "../src/processor/Processor.sol";
+import {ProcessorErrors} from "../src/processor/libs/ProcessorErrors.sol";
 
 contract ProcessorTest is Test {
+    // Main contract instance to be tested
     Processor public processor;
-    bytes32 public constant MOCK_AUTH_CONTRACT = bytes32(uint256(1));
-    address public constant MOCK_MAILBOX = address(0x1234);
+    // Mock mailbox address that will be authorized to call the processor
+    address public constant MAILBOX = address(0x1234);
+    // Mock authorization contract address converted to bytes32 for cross-chain representation
+    bytes32 public constant AUTH_CONTRACT = bytes32(uint256(uint160(address(0x5678))));
+    // Domain ID of the origin domain
+    uint32 public constant ORIGIN_DOMAIN = 1;
+    // Authorized addresses that can call the processor directly
+    address[] public AUTHORIZED_ADDRESSES = [address(0x1234)];
 
+    /// @notice Deploy a fresh instance of the processor before each test
     function setUp() public {
-        processor = new Processor(MOCK_AUTH_CONTRACT, MOCK_MAILBOX);
+        processor = new Processor(AUTH_CONTRACT, MAILBOX, ORIGIN_DOMAIN, AUTHORIZED_ADDRESSES);
     }
 
-    function testConstructorSuccess() public view {
-        assertEq(processor.authorizationContract(), MOCK_AUTH_CONTRACT, "Authorization contract not set correctly");
-        assertEq(processor.mailbox(), MOCK_MAILBOX, "Mailbox address not set correctly");
-        assertEq(processor.paused(), false, "Processor should not be paused initially");
+    /// @notice Test that the constructor properly initializes state variables
+    function testConstructor() public view {
+        assertEq(address(processor.mailbox()), MAILBOX);
+        assertEq(processor.authorizationContract(), AUTH_CONTRACT);
+        assertEq(processor.originDomain(), ORIGIN_DOMAIN);
+        assertEq(processor.authorizedAddresses(AUTHORIZED_ADDRESSES[0]), true);
+        assertFalse(processor.paused());
     }
 
-    function testConstructorZeroMailbox() public {
-        vm.expectRevert(Processor.InvalidAddress.selector);
-        new Processor(MOCK_AUTH_CONTRACT, address(0));
+    /// @notice Test that constructor reverts when given zero address for mailbox
+    function testConstructorRevertOnZeroMailbox() public {
+        vm.expectRevert(ProcessorErrors.InvalidAddress.selector);
+        new Processor(AUTH_CONTRACT, address(0), ORIGIN_DOMAIN, AUTHORIZED_ADDRESSES);
     }
 }
