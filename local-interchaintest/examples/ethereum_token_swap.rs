c08259c8afcbad94a8ca0bafb23f550fb29c4712
keyne <kleuyckx@gmail.com>
2025-01-07 18:00:03 +0000

    clean up

--- local-interchaintest/examples/ethereum_token_swap.rs
+++ local-interchaintest/examples/ethereum_token_swap.rs
@@ -1,6 +1,6 @@
-use std::{error::Error, str::FromStr};
+use std::error::Error;
 
-use alloy::primitives::Address;
+use alloy::{network::TransactionBuilder, primitives::U256, rpc::types::TransactionRequest};
 use local_interchaintest::utils::ethereum::EthClient;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -11,12 +11,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     let block = eth.get_block_number()?;
     println!("Current block number: {}", block);
 
-    // Address to check (this is the first Anvil test account)
-    let address = Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")?;
+    let accounts = eth.get_accounts_addresses()?;
+    println!("Accounts: {:?}", accounts);
 
     // Get balance
-    let balance = eth.get_balance(address)?;
+    let balance = eth.get_balance(accounts[0])?;
     println!("Balance: {} wei", balance);
+
+    let account = eth.get_account(accounts[0])?;
+    println!("Account 0: {:?}", account);
+
+    let balance_account_0_before = eth.get_balance(accounts[0])?;
+    println!("Balance account 0 before: {} wei", balance_account_0_before);
+    let balance_account_1_before = eth.get_balance(accounts[1])?;
+    println!("Balance account 1 before: {} wei", balance_account_1_before);
+
+    let tx = TransactionRequest::default()
+        .from(accounts[0])
+        .to(accounts[1])
+        .with_value(U256::from(100));
+    let hash = eth.send_transaction(tx)?;
+    println!("Transaction hash: {}", hash);
+
+    let balance_account_0_after = eth.get_balance(accounts[0])?;
+    println!("Balance account 0 after: {} wei", balance_account_0_after);
+    let balance_account_1_after = eth.get_balance(accounts[1])?;
+    println!("Balance account 1 after: {} wei", balance_account_1_after);
 
     Ok(())
 }
--- local-interchaintest/src/utils/ethereum.rs
+++ local-interchaintest/src/utils/ethereum.rs
@@ -1,12 +1,16 @@
 use alloy::{
-    primitives::{Address, U256}, providers::{Provider, ProviderBuilder, RootProvider}, transports::http::{reqwest::Url, Client, Http}
+    consensus::Account,
+    primitives::{Address, FixedBytes, U256},
+    providers::{Provider, ProviderBuilder},
+    rpc::types::TransactionRequest,
+    transports::http::{reqwest::Url, Client, Http},
 };
 use std::error::Error;
 use tokio::runtime::Runtime;
 
 pub struct EthClient {
-    provider: RootProvider<Http<Client>>,
-    rt: Runtime,
+    pub provider: Box<dyn Provider<Http<Client>>>,
+    pub rt: Runtime,
 }
 
 impl EthClient {
@@ -16,23 +20,56 @@ impl EthClient {
             .build()?;
 
         let url = Url::parse(url)?;
-        let provider = ProviderBuilder::new().on_http(url);
+        let provider = ProviderBuilder::new()
+            // Adds the `ChainIdFiller`, `GasFiller` and the `NonceFiller` layers.
+            // This is the recommended way to set up the provider.
+            .with_recommended_fillers()
+            .on_http(url);
 
-        Ok(Self { provider, rt })
+        Ok(Self {
+            provider: Box::new(provider),
+            rt,
+        })
     }
 
     pub fn get_block_number(&self) -> Result<u64, Box<dyn Error>> {
-        let number = self
-            .rt
-            .block_on(self.provider.get_block_number())?;
+        let number = self.rt.block_on(self.provider.get_block_number())?;
         Ok(number)
     }
-    
+
     pub fn get_balance(&self, address: Address) -> Result<U256, Box<dyn Error>> {
         let balance = self.rt.block_on(async {
             let balance = self.provider.get_balance(address).await;
             balance
         })?;
         Ok(balance)
+    }
+
+    pub fn get_accounts_addresses(&self) -> Result<Vec<Address>, Box<dyn Error>> {
+        let accounts = self.rt.block_on(async {
+            let accounts = self.provider.get_accounts().await;
+            accounts
+        })?;
+        Ok(accounts)
+    }
+
+    pub fn get_account(&self, address: Address) -> Result<Account, Box<dyn Error>> {
+        let account = self.rt.block_on(async {
+            let account = self.provider.get_account(address).await;
+            account
+        })?;
+        Ok(account)
+    }
+
+    pub fn send_transaction(&self, tx: TransactionRequest) -> Result<FixedBytes<32>, Box<dyn Error>> {
+        let tx_hash = self
+            .rt
+            .block_on(async {
+                let tx_hash = self.provider.send_transaction(tx).await;
+                tx_hash
+            })?
+            .tx_hash()
+            .clone();
+        Ok(tx_hash)
     }
 }
