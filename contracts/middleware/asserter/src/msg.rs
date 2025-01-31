d65eb8a9b8cd9f5b0037862fe80b8076b17ff66e
bekauz <bekauz@protonmail.com>
2025-01-31 14:31:59 +0100

    cleanup; add comments

--- contracts/middleware/asserter/src/msg.rs
+++ contracts/middleware/asserter/src/msg.rs
@@ -23,6 +23,18 @@ pub enum Predicate {
     GTE,
 }
 
+impl Predicate {
+    pub fn eval<T: PartialOrd + PartialEq>(&self, a: T, b: T) -> bool {
+        match self {
+            Predicate::LT => a < b,
+            Predicate::LTE => a <= b,
+            Predicate::EQ => a == b,
+            Predicate::GT => a > b,
+            Predicate::GTE => a >= b,
+        }
+    }
+}
+
 #[cw_serde]
 pub struct QueryInfo {
     // addr of the storage account
@@ -34,15 +46,15 @@ pub struct QueryInfo {
 }
 
 #[cw_serde]
-
 pub enum AssertionValue {
     // storage account slot query
     Variable(QueryInfo),
-    // serialized constant value
-    Constant(String),
+    // b64 encoded constant value
+    Constant(Binary),
 }
 
-// type that both values are expected to be
+/// supported evaluation types. both assertion values must be of this type
+/// in order to evaluate the condition.
 #[cw_serde]
 pub enum ValueType {
     Decimal,
