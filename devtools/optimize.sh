4a9f7b4a6b722da66f894a75a0cd34d754daf760
keyne <kleuyckx@gmail.com>
2025-02-18 21:37:31 +0000

    add solidity build to optimize script with docker

--- devtools/optimize.sh
+++ devtools/optimize.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $(uname -m) =~ "arm64" ]]; then \
+if [[ $(uname -m) =~ "arm64" ]]; then
     docker run --rm -v "$(pwd)":/code \
         --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
         --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
@@ -12,3 +12,9 @@ else
         --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
         cosmwasm/optimizer:0.16.0
 fi
+
+docker run --rm -it \
+    -v "$(pwd)/solidity":/solidity \
+    -w /solidity \
+    --entrypoint sh \
+    ghcr.io/foundry-rs/foundry:stable -c "forge soldeer install && forge build"
