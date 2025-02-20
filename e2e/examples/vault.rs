0b54d7f1b51df99488b5b61cfeb11c56fca60586
bekauz <bekauz@protonmail.com>
2025-02-20 15:34:24 +0100

    program manager neutron side setup

--- e2e/examples/vault.rs
+++ e2e/examples/vault.rs
@@ -4,20 +4,262 @@ use cosmwasm_std::{to_json_binary, Decimal};
 use localic_std::modules::cosmwasm::{contract_execute, contract_instantiate, contract_query};
 use localic_utils::{
     utils::{ethereum::EthClient, test_context::TestContext},
-    ConfigChainBuilder, TestContextBuilder, DEFAULT_KEY, LOCAL_IC_API_URL,
+    ConfigChainBuilder, TestContextBuilder, DEFAULT_KEY, GAIA_CHAIN_NAME, LOCAL_IC_API_URL,
     NEUTRON_CHAIN_ADMIN_ADDR, NEUTRON_CHAIN_DENOM, NEUTRON_CHAIN_NAME,
 };
 use log::info;
 use rand::{distributions::Alphanumeric, Rng};
 use serde_json::Value;
+use valence_astroport_lper::msg::LiquidityProviderConfig;
 use valence_astroport_utils::astroport_native_lp_token::{
-    AssetInfo, ConcentratedPoolParams, FactoryInstantiateMsg, FactoryQueryMsg, FeeShareConfig,
+    AssetInfo, ConcentratedPoolParams, FactoryInstantiateMsg, FactoryQueryMsg,
     NativeCoinRegistryExecuteMsg, NativeCoinRegistryInstantiateMsg, PairConfig, PairType,
 };
-use valence_e2e::utils::{
-    ethereum::set_up_anvil_container, ASTROPORT_PATH, DEFAULT_ANVIL_RPC_ENDPOINT, GAS_FLAGS,
-    LOCAL_CODE_ID_CACHE_PATH_NEUTRON, LOGS_FILE_PATH, NTRN_DENOM, VALENCE_ARTIFACTS_PATH,
+use valence_authorization_utils::{
+    authorization_message::{Message, MessageDetails, MessageType},
+    builders::{AtomicFunctionBuilder, AtomicSubroutineBuilder, AuthorizationBuilder},
+    domain::Domain,
 };
+use valence_e2e::utils::{
+    ethereum::set_up_anvil_container,
+    manager::{setup_manager, use_manager_init, ASTROPORT_LPER_NAME, ASTROPORT_WITHDRAWER_NAME},
+    ASTROPORT_PATH, DEFAULT_ANVIL_RPC_ENDPOINT, GAS_FLAGS, LOCAL_CODE_ID_CACHE_PATH_NEUTRON,
+    LOGS_FILE_PATH, NEUTRON_CONFIG_FILE, NTRN_DENOM, VALENCE_ARTIFACTS_PATH,
+};
+use valence_library_utils::liquidity_utils::AssetData;
+use valence_program_manager::{
+    account::{AccountInfo, AccountType},
+    library::LibraryInfo,
+    program_config_builder::ProgramConfigBuilder,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    // Start anvil container
+    let rt = tokio::runtime::Runtime::new()?;
+    rt.block_on(set_up_anvil_container())?;
+
+    let eth = EthClient::new(DEFAULT_ANVIL_RPC_ENDPOINT)?;
+
+    let mut test_ctx = TestContextBuilder::default()
+        .with_unwrap_raw_logs(true)
+        .with_api_url(LOCAL_IC_API_URL)
+        .with_artifacts_dir(VALENCE_ARTIFACTS_PATH)
+        .with_chain(ConfigChainBuilder::default_neutron().build()?)
+        .with_log_file_path(LOGS_FILE_PATH)
+        .build()?;
+
+    info!("creating subdenom to pair with NTRN");
+    // Let's create a token to pair it with NTRN
+    let token_subdenom: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .map(char::from)
+        .collect();
+
+    test_ctx
+        .build_tx_create_tokenfactory_token()
+        .with_subdenom(&token_subdenom)
+        .send()?;
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    let token = test_ctx
+        .get_tokenfactory_denom()
+        .creator(NEUTRON_CHAIN_ADMIN_ADDR)
+        .subdenom(token_subdenom)
+        .get();
+
+    // Mint some of the token
+    test_ctx
+        .build_tx_mint_tokenfactory_token()
+        .with_amount(1_000_000_000)
+        .with_denom(&token)
+        .send()
+        .unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    // setup astroport
+    let (
+        astroport_factory_code_id,
+        astroport_pair_concentrated_code_id,
+        astroport_token_code_id,
+        astroport_coin_registry_code_id,
+    ) = deploy_astroport_contracts(&mut test_ctx)?;
+
+    let (pool_addr, lp_token) = setup_astroport_cl_pool(
+        &mut test_ctx,
+        astroport_pair_concentrated_code_id,
+        astroport_token_code_id,
+        astroport_factory_code_id,
+        astroport_coin_registry_code_id,
+        token.to_string(),
+    )?;
+
+    // setup neutron side:
+    // 1. authorizations
+    // 2. processor
+    // 3. astroport LP & LW
+    // 4. base account
+    setup_manager(
+        &mut test_ctx,
+        NEUTRON_CONFIG_FILE,
+        vec![GAIA_CHAIN_NAME],
+        vec![ASTROPORT_LPER_NAME, ASTROPORT_WITHDRAWER_NAME],
+    )?;
+
+    let mut builder = ProgramConfigBuilder::new(NEUTRON_CHAIN_ADMIN_ADDR.to_string());
+    let ntrn_domain =
+        valence_program_manager::domain::Domain::CosmosCosmwasm(NEUTRON_CHAIN_NAME.to_string());
+
+    let deposit_account_info =
+        AccountInfo::new("deposit".to_string(), &ntrn_domain, AccountType::default());
+
+    let position_account_info =
+        AccountInfo::new("position".to_string(), &ntrn_domain, AccountType::default());
+
+    let withdraw_account_info =
+        AccountInfo::new("withdraw".to_string(), &ntrn_domain, AccountType::default());
+
+    let deposit_acc = builder.add_account(deposit_account_info);
+    let position_acc = builder.add_account(position_account_info);
+    let withdraw_acc = builder.add_account(withdraw_account_info);
+
+    let astro_cl_pair_type = valence_astroport_utils::astroport_native_lp_token::PairType::Custom(
+        "concentrated".to_string(),
+    );
+
+    let astro_cl_pool_asset_data = AssetData {
+        asset1: NTRN_DENOM.to_string(),
+        asset2: token.clone(),
+    };
+
+    let astro_lp_config = LiquidityProviderConfig {
+        pool_type: valence_astroport_utils::PoolType::NativeLpToken(astro_cl_pair_type.clone()),
+        asset_data: astro_cl_pool_asset_data.clone(),
+        max_spread: None,
+    };
+
+    let astro_lw_config = valence_astroport_withdrawer::msg::LiquidityWithdrawerConfig {
+        pool_type: valence_astroport_utils::PoolType::NativeLpToken(astro_cl_pair_type),
+        asset_data: astro_cl_pool_asset_data.clone(),
+    };
+
+    let astro_lper_library_cfg = valence_astroport_lper::msg::LibraryConfig {
+        input_addr: deposit_acc.clone(),
+        output_addr: position_acc.clone(),
+        lp_config: astro_lp_config,
+        pool_addr: pool_addr.to_string(),
+    };
+    let astro_lwer_library_cfg = valence_astroport_withdrawer::msg::LibraryConfig {
+        input_addr: position_acc.clone(),
+        output_addr: withdraw_acc.clone(),
+        withdrawer_config: astro_lw_config,
+        pool_addr: pool_addr.to_string(),
+    };
+
+    let astro_lper_library = builder.add_library(LibraryInfo::new(
+        "astro_lp".to_string(),
+        &ntrn_domain,
+        valence_program_manager::library::LibraryConfig::ValenceAstroportLper(
+            astro_lper_library_cfg,
+        ),
+    ));
+
+    let astro_lwer_library = builder.add_library(LibraryInfo::new(
+        "astro_lw".to_string(),
+        &ntrn_domain,
+        valence_program_manager::library::LibraryConfig::ValenceAstroportWithdrawer(
+            astro_lwer_library_cfg,
+        ),
+    ));
+
+    // establish the deposit_acc -> lper_lib -> position_acc link
+    builder.add_link(&astro_lper_library, vec![&deposit_acc], vec![&position_acc]);
+    // establish the position_acc -> lwer_lib -> withdraw_acc link
+    builder.add_link(
+        &astro_lwer_library,
+        vec![&position_acc],
+        vec![&withdraw_acc],
+    );
+
+    let astro_lper_function = AtomicFunctionBuilder::new()
+        .with_domain(Domain::Main)
+        .with_contract_address(astro_lper_library.clone())
+        .with_message_details(MessageDetails {
+            message_type: MessageType::CosmwasmExecuteMsg,
+            message: Message {
+                name: "process_function".to_string(),
+                params_restrictions: None,
+            },
+        })
+        .build();
+
+    let astro_lwer_function = AtomicFunctionBuilder::new()
+        .with_domain(Domain::Main)
+        .with_contract_address(astro_lwer_library.clone())
+        .with_message_details(MessageDetails {
+            message_type: MessageType::CosmwasmExecuteMsg,
+            message: Message {
+                name: "process_function".to_string(),
+                params_restrictions: None,
+            },
+        })
+        .build();
+
+    builder.add_authorization(
+        AuthorizationBuilder::new()
+            .with_label("provide_liquidity")
+            .with_subroutine(
+                AtomicSubroutineBuilder::new()
+                    .with_function(astro_lper_function)
+                    .build(),
+            )
+            .build(),
+    );
+    builder.add_authorization(
+        AuthorizationBuilder::new()
+            .with_label("withdraw_liquidity")
+            .with_subroutine(
+                AtomicSubroutineBuilder::new()
+                    .with_function(astro_lwer_function)
+                    .build(),
+            )
+            .build(),
+    );
+
+    let mut program_config = builder.build();
+
+    info!("initializing manager...");
+    use_manager_init(&mut program_config)?;
+
+    let deposit_acc_addr = program_config
+        .get_account(deposit_acc)?
+        .addr
+        .clone()
+        .unwrap();
+    let position_acc_addr = program_config
+        .get_account(position_acc)?
+        .addr
+        .clone()
+        .unwrap();
+    let withdraw_acc_addr = program_config
+        .get_account(withdraw_acc)?
+        .addr
+        .clone()
+        .unwrap();
+
+    info!("DEPOSIT ACCOUNT\t: {deposit_acc_addr}");
+    info!("POSITION ACCOUNT\t: {position_acc_addr}");
+    info!("WITHDRAW ACCOUNT\t: {withdraw_acc_addr}");
+
+    // setup eth side:
+    // 1. base account
+    // 2. vault
+    // 3. lite processor
+
+    Ok(())
+}
 
 fn deploy_astroport_contracts(
     test_ctx: &mut TestContext,
@@ -74,36 +316,8 @@ fn setup_astroport_cl_pool(
     token_code_id: u64,
     factory_code_id: u64,
     native_coin_registry_code_id: u64,
+    denom: String,
 ) -> Result<(String, String), Box<dyn Error>> {
-    info!("creating subdenom to pair with NTRN");
-    // Let's create a token to pair it with NTRN
-    let token_subdenom: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(10)
-        .map(char::from)
-        .collect();
-
-    test_ctx
-        .build_tx_create_tokenfactory_token()
-        .with_subdenom(&token_subdenom)
-        .send()?;
-    std::thread::sleep(std::time::Duration::from_secs(3));
-
-    let token = test_ctx
-        .get_tokenfactory_denom()
-        .creator(NEUTRON_CHAIN_ADMIN_ADDR)
-        .subdenom(token_subdenom)
-        .get();
-
-    // Mint some of the token
-    test_ctx
-        .build_tx_mint_tokenfactory_token()
-        .with_amount(1_000_000_000)
-        .with_denom(&token)
-        .send()
-        .unwrap();
-    std::thread::sleep(std::time::Duration::from_secs(3));
-
     info!("Instantiating astroport native coin registry...");
     let coin_registry_contract = contract_instantiate(
         test_ctx
@@ -134,7 +348,7 @@ fn setup_astroport_cl_pool(
         &coin_registry_contract.address,
         DEFAULT_KEY,
         &serde_json::to_string(&NativeCoinRegistryExecuteMsg::Add {
-            native_coins: vec![(NEUTRON_CHAIN_DENOM.to_string(), 6), (token.to_string(), 6)],
+            native_coins: vec![(NEUTRON_CHAIN_DENOM.to_string(), 6), (denom.to_string(), 6)],
         })
         .unwrap(),
         GAS_FLAGS,
@@ -186,7 +400,7 @@ fn setup_astroport_cl_pool(
             denom: NTRN_DENOM.to_string(),
         },
         AssetInfo::NativeToken {
-            denom: token.clone(),
+            denom: denom.clone(),
         },
     ];
 
@@ -247,51 +461,4 @@ fn setup_astroport_cl_pool(
     info!("Pool created successfully! Pool address: {pool_addr}, LP token: {lp_token}");
 
     Ok((pool_addr.to_string(), lp_token.to_string()))
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::init();
-
-    // Start anvil container
-    let rt = tokio::runtime::Runtime::new()?;
-    rt.block_on(set_up_anvil_container())?;
-
-    let eth = EthClient::new(DEFAULT_ANVIL_RPC_ENDPOINT)?;
-
-    let mut test_ctx = TestContextBuilder::default()
-        .with_unwrap_raw_logs(true)
-        .with_api_url(LOCAL_IC_API_URL)
-        .with_artifacts_dir(VALENCE_ARTIFACTS_PATH)
-        .with_chain(ConfigChainBuilder::default_neutron().build()?)
-        .with_log_file_path(LOGS_FILE_PATH)
-        .build()?;
-
-    // setup astroport
-    let (
-        astroport_factory_code_id,
-        astroport_pair_concentrated_code_id,
-        astroport_token_code_id,
-        astroport_coin_registry_code_id,
-    ) = deploy_astroport_contracts(&mut test_ctx)?;
-
-    let (pool_addr, lp_token) = setup_astroport_cl_pool(
-        &mut test_ctx,
-        astroport_pair_concentrated_code_id,
-        astroport_token_code_id,
-        astroport_factory_code_id,
-        astroport_coin_registry_code_id,
-    )?;
-
-    // setup neutron side:
-    // 1. authorizations
-    // 2. processor
-    // 3. astroport LP & LW
-    // 4. base account
-
-    // setup eth side:
-    // 1. base account
-    // 2. vault
-    // 3. lite processor
-
-    Ok(())
 }
