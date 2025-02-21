f37f9f23016521486af6ea4e4ffab6528bb429c8
udit <udit.vira@gmail.com>
2025-02-21 16:16:21 -0500

    add token swap example comments

--- examples/token_swap/src/token_swap.rs
+++ examples/token_swap/src/token_swap.rs
@@ -13,6 +13,21 @@ use valence_program_manager::{
 };
 use valence_splitter_library::msg::{UncheckedSplitAmount, UncheckedSplitConfig};
 
+/// Returns a `ProgramConfig` for a program that atomically swap tokens 
+/// between two program controlled accounts on a given domain. 
+/// This `ProgramConfig` can be used to deploy and configure the program. 
+/// The accompanying token_swap_test.rs demonstrates local deployment and testing of 
+/// this program.
+///
+/// # Arguments
+/// * `domain` - the domain of type `Domain` on which the swap occurs (e.g Neutron)
+/// * `owner` - the owner of the deployed program
+/// * `token1` - the token denomination required in the first account
+/// * `token2` - the token denomination required in the second account
+/// * `swap_amount_token1` - the amount of the first token to swap
+/// * `swap_amount_token2` - the amount of the second to to swap
+/// * `authorized_swap_party` - the party that receives the authority token to execute
+///                             program
 pub fn my_atomic_token_swap_program(
     domain: Domain,
     owner: String,
@@ -24,6 +39,7 @@ pub fn my_atomic_token_swap_program(
 ) -> ProgramConfig {
     let mut program_config_builder = ProgramConfigBuilder::new(owner);
 
+    // Create two accounts in the domain
     let account_1 = program_config_builder.add_account(AccountInfo::new(
         "base_account_1".to_string(),
         &domain,
@@ -36,6 +52,11 @@ pub fn my_atomic_token_swap_program(
         AccountType::default(),
     ));
 
+    // Valence provides a splitter library that can send tokens from 1 accounts to N other
+    // accounts. In the atomic swap example, two instances of the library are configured.
+
+    // The first splitter instance is configured to send a fixed `swap_amount_token1` from 
+    // the first account to the second account.
     let library_1 = program_config_builder.add_library(LibraryInfo::new(
         "splitter_1".to_string(),
         &domain,
@@ -48,7 +69,8 @@ pub fn my_atomic_token_swap_program(
             }],
         }),
     ));
-
+    // The second splitter instance is configured to send a fixed `swap_amount_token2`
+    // from the second account to the the first account.
     let library_2 = program_config_builder.add_library(LibraryInfo::new(
         "splitter_2".to_string(),
         &domain,
@@ -62,9 +84,11 @@ pub fn my_atomic_token_swap_program(
         }),
     ));
 
+    // The two splitter instances are registered with the two accounts.
     program_config_builder.add_link(&library_1, vec![&account_1], vec![&account_2]);
     program_config_builder.add_link(&library_2, vec![&account_2], vec![&account_1]);
 
+    // A function is built to call the split function on the first splitter.
     let send_tokens_from_account1_to_account2 = AtomicFunctionBuilder::new()
         .with_contract_address(library_1.clone())
         .with_message_details(MessageDetails {
@@ -79,6 +103,7 @@ pub fn my_atomic_token_swap_program(
         })
         .build();
 
+    // A function is built do call the split function on the second splitter.
     let send_tokens_from_account2_to_account1 = AtomicFunctionBuilder::new()
         .with_contract_address(library_2.clone())
         .with_message_details(MessageDetails {
@@ -93,11 +118,18 @@ pub fn my_atomic_token_swap_program(
         })
         .build();
 
+    // To make this swap occur atomically, the two functions are combined into
+    // an atomic subroutine.
     let atomic_subroutine = AtomicSubroutineBuilder::new()
         .with_function(send_tokens_from_account1_to_account2)
         .with_function(send_tokens_from_account2_to_account1)
         .build();
 
+    // An authorization is generated that will create an authority token `atomic_swap`
+    // and give the authority token to the `authorized_swap_party`. This party
+    // is given the ability to invoke the `atomic_subroutine` created previously.
+    // The `PermissionTypeInfo::WithoutCallLimit` signifies that the party can invoke
+    // the subroutine an indefinite number of times.  
     let authorization = AuthorizationBuilder::new()
         .with_label("atomic_swap")
         .with_mode(AuthorizationModeInfo::Permissioned(
@@ -107,6 +139,7 @@ pub fn my_atomic_token_swap_program(
         .build();
 
     program_config_builder.add_authorization(authorization);
-
+    
+    // The program config is built.
     program_config_builder.build()
 }
