005523059c7d47d782f92a40ddd6474ffa535c5b
Art3mix <r3mix.il@gmail.com>
2025-03-04 22:49:09 +0200

    remove admin check from registry

--- contracts/program-registry/src/error.rs
+++ contracts/program-registry/src/error.rs
@@ -14,4 +14,9 @@ pub enum ContractError {
     ProgramAlreadyExists(u64),
     #[error("Program doesn't exists with id {0}")]
     ProgramDoesntExists(u64),
+
+    #[error("Unauthorized: This id is reserved to {0}")]
+    UnauthorizedToSave(String),
+    #[error("Unauthorized: This id can be only updated by {0}")]
+    UnauthorizedToUpdate(String),
 }
--- contracts/program-registry/src/state.rs
+++ contracts/program-registry/src/state.rs
@@ -1,6 +1,7 @@
-use cosmwasm_std::Binary;
+use cosmwasm_std::{Addr, Binary};
 use cw_storage_plus::{Item, Map};
 
 pub const LAST_ID: Item<u64> = Item::new("id");
+pub const PROGRAMS_OWNERS: Map<u64, Addr> = Map::new("programs_owners");
 pub const PROGRAMS: Map<u64, Binary> = Map::new("programs");
 pub const PROGRAMS_BACKUP: Map<u64, Binary> = Map::new("programs_backups");
