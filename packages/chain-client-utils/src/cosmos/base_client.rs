e03839bf441929fcba571ef76e5ab4fa1f984262
bekauz <bekauz@protonmail.com>
2025-03-05 15:30:03 +0100

    add transaction response try_from broadcasting response

--- packages/chain-client-utils/src/cosmos/base_client.rs
+++ packages/chain-client-utils/src/cosmos/base_client.rs
@@ -60,10 +60,7 @@ pub trait BaseClient: GrpcSigningClient {
 
         let broadcast_tx_response = grpc_client.broadcast_tx(raw_tx).await?.into_inner();
 
-        match broadcast_tx_response.tx_response {
-            Some(tx_response) => Ok(TransactionResponse::try_from(tx_response)?),
-            None => Err(StrategistError::TransactionError("failed".to_string())),
-        }
+        TransactionResponse::try_from(broadcast_tx_response.tx_response)
     }
 
     async fn latest_block_header(&self) -> Result<Header, StrategistError> {
@@ -197,9 +194,6 @@ pub trait BaseClient: GrpcSigningClient {
 
         let broadcast_tx_response = grpc_client.broadcast_tx(raw_tx).await?.into_inner();
 
-        match broadcast_tx_response.tx_response {
-            Some(tx_response) => Ok(TransactionResponse::try_from(tx_response)?),
-            None => Err(StrategistError::TransactionError("failed".to_string())),
-        }
+        TransactionResponse::try_from(broadcast_tx_response.tx_response)
     }
 }
