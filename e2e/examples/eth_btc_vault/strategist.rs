635a279ef591e9d634a6ae4eadd61f8e0d737f5b
bekauz <bekauz@protonmail.com>
2025-05-30 17:54:34 +0200

    wip: strategy flow outline

--- e2e/examples/eth_btc_vault/strategist.rs
+++ e2e/examples/eth_btc_vault/strategist.rs
@@ -6,8 +6,8 @@ use log::info;
 use valence_domain_clients::evm::request_provider_client::RequestProviderClient;
 use valence_e2e::utils::{
     solidity_contracts::{
-        sol_authorizations::Authorizations, sol_lite_processor::LiteProcessor, BaseAccount,
-        IBCEurekaTransfer, OneWayVault, ERC20,
+        sol_authorizations::Authorizations, sol_lite_processor::LiteProcessor, IBCEurekaTransfer,
+        OneWayVault, ERC20,
     },
     worker::ValenceWorker,
 };
@@ -37,10 +37,7 @@ impl ValenceWorker for Strategy {
         );
         let eth_processor_contract =
             LiteProcessor::new(Address::from_str(&self.cfg.ethereum.processor)?, &eth_rp);
-        let eth_deposit_acc_contract = BaseAccount::new(
-            Address::from_str(&self.cfg.ethereum.accounts.deposit)?,
-            &eth_rp,
-        );
+
         let eth_wbtc_contract =
             ERC20::new(Address::from_str(&self.cfg.ethereum.denoms.wbtc)?, &eth_rp);
         let eth_one_way_vault_contract = OneWayVault::new(
@@ -51,6 +48,88 @@ impl ValenceWorker for Strategy {
             Address::from_str(&self.cfg.ethereum.libraries.eureka_forwarder)?,
             &eth_rp,
         );
+
+        // first we carry out the deposit flow
+        self.deposit().await?;
+
+        // after deposit flow is complete, we process the new obligations
+        self.register_withdraw_obligations().await?;
+
+        // with new obligations registered into the clearing queue, we
+        // carry out the settlements
+        self.settlement().await?;
+
+        Ok(())
+    }
+}
+
+impl Strategy {
+    /// carries out the steps needed to bring the new deposits from Ethereum to
+    /// Neutron (via Cosmos Hub) before depositing them into Mars protocol.
+    async fn deposit(&mut self) -> Result<(), Box<dyn Error + Send + Sync>> {
+        // 1. query the ethereum deposit account balance
+
+        // 2. validate that the deposit account balance exceeds the eureka routing
+        // threshold amount (from cfg)
+
+        // 3. perform IBC-Eureka transfer to Cosmos Hub ICA
+
+        // 4. block execution until the funds arrive to the Cosmos Hub ICA owned
+        // by the Valence Interchain Account on Neutron
+
+        // 5. Initiate ICA-IBC-Transfer from Cosmos Hub ICA to Neutron program
+        // deposit account
+
+        // 6. block execution until funds arrive to the Neutron program deposit
+        // account
+
+        // 7. use Valence Forwarder to route funds from the Neutron program
+        // deposit account to the Mars deposit account
+
+        // 8. use Mars Lending library to deposit funds from Mars deposit account
+        // into Mars protocol
+
+        Ok(())
+    }
+
+    /// reads the newly submitted withdrawal obligations that are not yet
+    /// present in the Clearing Queue, generates their zero-knowledge proofs,
+    /// and posts them into the Clearing queue in order.
+    async fn register_withdraw_obligations(&mut self) -> Result<(), Box<dyn Error + Send + Sync>> {
+        // 1. query the Clearing Queue library for the latest posted withdraw request ID
+
+        // 2. query the OneWayVault for emitted events and filter them such that
+        // only requests with id greater than the one queried in step 1. are fetched
+
+        // 3. process the new OneWayVault Withdraw events in order from the oldest
+        // to the newest, posting them to the coprocessor to obtain a ZKP
+
+        // 4. preserving the order, post the ZKPs obtained in step 3. to the Neutron
+        // Authorizations contract, enqueuing them to the processor
+
+        // 5. tick the processor to register the obligations to the clearing queue
+
+        Ok(())
+    }
+
+    /// performs the final settlement of registered withdrawal obligations in
+    /// the Clearing Queue library. this involves topping up the settlement
+    /// account with funds necessary to carry out all withdrawal obligations
+    /// in the queue.
+    async fn settlement(&mut self) -> Result<(), Box<dyn Error + Send + Sync>> {
+        // 1. query the current settlement account balance
+
+        // 2. query the Clearing Queue and calculate the total active obligations
+
+        // 3. simulate Mars protocol withdrawal to obtain the funds necessary
+        // to fulfill all active withdrawal requests
+
+        // 4. call the Mars lending library to perform the withdrawal.
+        // This will deposit the underlying assets directly to the settlement account.
+
+        // 5. queue the Clearing Queue settlement requests to the processor
+
+        // 6. tick the processor until all withdraw obligations are settled
 
         Ok(())
     }
