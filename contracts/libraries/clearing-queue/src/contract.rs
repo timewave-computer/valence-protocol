91bb2e27311a36b85564ea2d4f50b95242297c14
bekauz <bekauz@protonmail.com>
2025-06-02 20:24:26 +0200

    feat: clearing queue v2 (#368)

--- contracts/libraries/clearing-queue/src/contract.rs
+++ contracts/libraries/clearing-queue/src/contract.rs
@@ -8,11 +8,8 @@ use valence_library_utils::{
 };
 
 use crate::{
-    msg::{
-        Config, FunctionMsgs, LibraryConfig, LibraryConfigUpdate, ObligationStatusResponse,
-        QueryMsg,
-    },
-    state::REGISTERED_OBLIGATION_IDS,
+    msg::{Config, FunctionMsgs, LibraryConfig, LibraryConfigUpdate, QueryMsg},
+    state::OBLIGATION_ID_TO_STATUS_MAP,
 };
 
 // version info for migration info
@@ -63,13 +60,17 @@ mod execute {
 }
 
 mod functions {
-    use cosmwasm_std::{ensure, BankMsg, Coin, DepsMut, Env, MessageInfo, Response, Uint64};
+    use cosmwasm_std::{
+        ensure, BankMsg, Coin, DepsMut, Env, MessageInfo, Response, Uint128, Uint64,
+    };
 
     use valence_library_utils::{error::LibraryError, execute_on_behalf_of};
 
     use crate::{
         msg::{Config, FunctionMsgs},
-        state::{WithdrawalObligation, CLEARING_QUEUE, REGISTERED_OBLIGATION_IDS},
+        state::{
+            ObligationStatus, WithdrawalObligation, CLEARING_QUEUE, OBLIGATION_ID_TO_STATUS_MAP,
+        },
     };
 
     pub fn process_function(
@@ -82,9 +83,9 @@ mod functions {
         match msg {
             FunctionMsgs::RegisterObligation {
                 recipient,
-                payout_coins,
+                payout_amount,
                 id,
-            } => try_register_withdraw_obligation(deps, env, cfg, recipient, payout_coins, id),
+            } => try_register_withdraw_obligation(deps, env, cfg, recipient, payout_amount, id),
             FunctionMsgs::SettleNextObligation {} => try_settle_next_obligation(deps, cfg),
         }
     }
@@ -92,41 +93,67 @@ mod functions {
     fn try_register_withdraw_obligation(
         deps: DepsMut,
         env: Env,
-        _cfg: Config,
+        mut cfg: Config,
         recipient: String,
-        payout_coins: Vec<Coin>,
+        payout_amount: Uint128,
         id: Uint64,
     ) -> Result<Response, LibraryError> {
-        // validate that this obligation is not registered yet
+        // increment the latest obligation id to the expected value
+        cfg.latest_id = cfg
+            .latest_id
+            .checked_add(Uint64::one())
+            .map_err(|_| LibraryError::ExecutionError("id overflow".to_string()))?;
+
+        // save the config with the incremented id
+        valence_library_base::save_config(deps.storage, &cfg)?;
+
+        // validate that id of the obligation being registered is monotonically increasing
         ensure!(
-            !REGISTERED_OBLIGATION_IDS.has(deps.storage, id.u64()),
+            cfg.latest_id == id,
             LibraryError::ExecutionError(format!(
-                "obligation #{id} is already registered in the queue"
+                "obligation being registered id out of order: expected {}, got {id}",
+                cfg.latest_id
             ))
         );
 
-        // obligation payouts cannot be empty
-        ensure!(
-            !payout_coins.is_empty(),
-            LibraryError::ExecutionError(
-                "obligation must have payout coins in order to be registered".to_string()
-            )
-        );
+        // we validate the obligation recipient address:
+        // - if address is valid, we proceed
+        // - if address is invalid, we immediately mark the obligation as processed
+        // with an error message to not block further obligations from being registered
+        let validated_recipient = match deps.api.addr_validate(&recipient) {
+            Ok(addr) => addr,
+            Err(e) => {
+                OBLIGATION_ID_TO_STATUS_MAP.save(
+                    deps.storage,
+                    id.u64(),
+                    &ObligationStatus::Error(e.to_string()),
+                )?;
+                return Ok(Response::default());
+            }
+        };
 
-        // each coin in the obligation to be paid out must have non-zero amount
-        for payout_coin in &payout_coins {
-            ensure!(
-                !payout_coin.amount.is_zero(),
-                LibraryError::ExecutionError(format!(
-                    "obligation payout coin {} amount cannot be zero",
-                    payout_coin.denom
-                ))
-            );
-        }
+        // we validate the payout amount:
+        // - if amount is non-zero, we proceed
+        // - if amount is zero, we immediately mark the obligation as processed
+        // with an error message to not block further obligations from being registered
+        let payout_coin = if !payout_amount.is_zero() {
+            Coin {
+                amount: payout_amount,
+                denom: cfg.denom.to_string(),
+            }
+        } else {
+            OBLIGATION_ID_TO_STATUS_MAP.save(
+                deps.storage,
+                id.u64(),
+                &ObligationStatus::Error("zero payout amount".to_string()),
+            )?;
+            return Ok(Response::default());
+        };
 
+        // construct the valid withdrawal obligation to be queued
         let withdraw_obligation = WithdrawalObligation {
-            recipient: deps.api.addr_validate(&recipient)?,
-            payout_coins,
+            recipient: validated_recipient,
+            payout_coin,
             id,
             enqueue_block: env.block,
         };
@@ -135,12 +162,9 @@ mod functions {
         CLEARING_QUEUE.push_back(deps.storage, &withdraw_obligation)?;
 
         // store the id of the registered obligation in the map with
-        // value `false` to indicate that this obligation is not yet
+        // value `InQueue` to indicate that this obligation is not yet
         // settled/complete.
-        // this map also serves as a check to prevent registering (and
-        // thus settling) the same obligation twice. because of this,
-        // upon settlement, the key remains - only the value is updated.
-        REGISTERED_OBLIGATION_IDS.save(deps.storage, id.u64(), &false)?;
+        OBLIGATION_ID_TO_STATUS_MAP.save(deps.storage, id.u64(), &ObligationStatus::InQueue)?;
 
         Ok(Response::new())
     }
@@ -158,39 +182,35 @@ mod functions {
             }
         };
 
-        let mut transfer_coins = vec![];
-
         // ensure that the settlement account is sufficiently topped up
         // to fulfill the obligation
-        for obligation_coin in obligation.payout_coins {
-            let settlement_acc_bal = deps.querier.query_balance(
-                cfg.settlement_acc_addr.as_str(),
-                obligation_coin.denom.to_string(),
-            )?;
+        let settlement_acc_bal = deps.querier.query_balance(
+            cfg.settlement_acc_addr.as_str(),
+            obligation.payout_coin.denom.to_string(),
+        )?;
 
-            ensure!(
-                settlement_acc_bal.amount >= obligation_coin.amount,
-                LibraryError::ExecutionError(format!(
-                    "insufficient settlement acc balance to fulfill obligation: {} < {}",
-                    settlement_acc_bal, obligation_coin
-                ))
-            );
-
-            // push the validated coin to be paid out
-            transfer_coins.push(obligation_coin);
-        }
+        ensure!(
+            settlement_acc_bal.amount >= obligation.payout_coin.amount,
+            LibraryError::ExecutionError(format!(
+                "insufficient settlement acc balance to fulfill obligation: {} < {}",
+                settlement_acc_bal, obligation.payout_coin
+            ))
+        );
 
         let fill_msg = BankMsg::Send {
             to_address: obligation.recipient.to_string(),
-            amount: transfer_coins,
+            amount: vec![obligation.payout_coin],
         };
 
         let input_account_msg =
             execute_on_behalf_of(vec![fill_msg.into()], &cfg.settlement_acc_addr)?;
 
-        // update the registered obligation entry value to `true` to indicate that
-        // this obligation had been settled.
-        REGISTERED_OBLIGATION_IDS.save(deps.storage, obligation.id.u64(), &true)?;
+        // mark the obligation as processed
+        OBLIGATION_ID_TO_STATUS_MAP.save(
+            deps.storage,
+            obligation.id.u64(),
+            &ObligationStatus::Processed,
+        )?;
 
         Ok(Response::new().add_message(input_account_msg))
     }
@@ -244,8 +264,8 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
             to_json_binary(&query::get_obligations(deps, from, to)?)
         }
         QueryMsg::ObligationStatus { id } => {
-            let settled = REGISTERED_OBLIGATION_IDS.load(deps.storage, id)?;
-            to_json_binary(&ObligationStatusResponse { settled })
+            let obligation_status = OBLIGATION_ID_TO_STATUS_MAP.load(deps.storage, id)?;
+            to_json_binary(&obligation_status)
         }
     }
 }
--- contracts/libraries/clearing-queue/src/testing/tests.rs
+++ contracts/libraries/clearing-queue/src/testing/tests.rs
@@ -1,12 +1,10 @@
-use cosmwasm_std::{coin, coins};
+use cosmwasm_std::{coin, Uint128};
 use valence_library_utils::LibraryAccountType;
 
 use crate::{
     msg::LibraryConfig,
-    testing::{
-        builder::ClearingQueueTestingSuiteBuilder,
-        suite::{DENOM_1, DENOM_2},
-    },
+    state::ObligationStatus,
+    testing::{builder::ClearingQueueTestingSuiteBuilder, suite::DENOM_1},
 };
 
 const INVALID_ADDR: &str = "invalid_addr";
@@ -29,6 +27,8 @@ fn test_update_validates_input_acc() {
     suite
         .update_clearing_config(LibraryConfig {
             settlement_acc_addr: new_settlement_acc_addr,
+            denom: "denom".to_string(),
+            latest_id: None,
         })
         .unwrap();
 }
@@ -43,61 +43,59 @@ fn test_settling_obligations_requires_nonempty_queue() {
 
 #[test]
 #[should_panic(
-    expected = "insufficient settlement acc balance to fulfill obligation: 100DENOM_2 < 150DENOM_2"
+    expected = "insufficient settlement acc balance to fulfill obligation: 100DENOM_1 < 150DENOM_1"
 )]
 fn test_settling_obligations_requires_funded_settlement_account() {
     let mut suite = ClearingQueueTestingSuiteBuilder::default()
-        .with_input_balances(vec![coin(100, DENOM_1), coin(100, DENOM_2)])
+        .with_input_balance(coin(100, DENOM_1))
         .build();
 
     suite
-        .register_new_obligation(
-            suite.user_1.to_string(),
-            vec![coin(100, DENOM_1), coin(150, DENOM_2)],
-            1,
-        )
+        .register_new_obligation(suite.user_1.to_string(), 150u128.into(), 1)
         .unwrap();
 
     suite.settle_next_obligation().unwrap();
 }
 
 #[test]
-#[should_panic(expected = "obligation must have payout coins in order to be registered")]
-fn test_registering_obligation_validates_payout_coins_len() {
-    let mut suite = ClearingQueueTestingSuiteBuilder::default().build();
+#[should_panic(expected = "no pending obligations")]
+fn test_processing_invalid_obligation_with_zero_amount_works() {
+    let mut suite = ClearingQueueTestingSuiteBuilder::default()
+        .with_input_balance(coin(1_000, DENOM_1))
+        .build();
 
     suite
-        .register_new_obligation(suite.user_1.to_string(), vec![], 1)
+        .register_new_obligation(suite.user_1.to_string(), Uint128::zero(), 1)
         .unwrap();
+
+    let obligation_status = suite.query_obligation_status(1);
+    assert!(matches!(obligation_status, ObligationStatus::Error(_)));
+
+    suite.settle_next_obligation().unwrap();
 }
 
 #[test]
-#[should_panic(expected = "Error decoding bech32")]
-fn test_registering_obligation_validates_recipient_addr() {
-    let mut suite = ClearingQueueTestingSuiteBuilder::default().build();
+#[should_panic(expected = "no pending obligations")]
+fn test_processing_invalid_obligation_with_invalid_addr_works() {
+    let mut suite = ClearingQueueTestingSuiteBuilder::default()
+        .with_input_balance(coin(1_000, DENOM_1))
+        .build();
 
     suite
-        .register_new_obligation(
-            "invalid_addr".to_string(),
-            vec![coin(100, DENOM_1), coin(150, DENOM_2)],
-            1,
-        )
+        .register_new_obligation("invalid_addr".to_string(), Uint128::new(100), 1)
         .unwrap();
-}
 
-#[test]
-#[should_panic(expected = "obligation payout coin DENOM_1 amount cannot be zero")]
-fn test_registering_obligation_validates_payout_coins_nonzero_amounts() {
-    let mut suite = ClearingQueueTestingSuiteBuilder::default().build();
+    let obligation_status = suite.query_obligation_status(1);
+    assert!(matches!(obligation_status, ObligationStatus::Error(_)));
 
-    suite
-        .register_new_obligation(suite.user_1.to_string(), coins(0, DENOM_1), 1)
-        .unwrap();
+    suite.settle_next_obligation().unwrap();
 }
 
 #[test]
 fn test_register_withdraw_obligation_happy() {
-    let mut suite = ClearingQueueTestingSuiteBuilder::default().build();
+    let mut suite = ClearingQueueTestingSuiteBuilder::default()
+        .with_latest_obligation_id(9)
+        .build();
 
     let queue_len_0 = suite.query_queue_info().len;
     let queue_resp_0 = suite.query_obligations(None, None);
@@ -106,7 +104,7 @@ fn test_register_withdraw_obligation_happy() {
     assert!(queue_resp_0.obligations.is_empty());
 
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(100, DENOM_1), 10)
+        .register_new_obligation(suite.user_1.to_string(), 100u128.into(), 10)
         .unwrap();
 
     let queue_len = suite.query_queue_info().len;
@@ -115,13 +113,13 @@ fn test_register_withdraw_obligation_happy() {
 
     assert_eq!(queue_len, 1);
     assert_eq!(queue_resp.obligations.len(), 1);
-    assert!(!obligation_status.settled);
+    assert!(matches!(obligation_status, ObligationStatus::InQueue));
 }
 
 #[test]
 fn test_queue_operates_in_fifo_manner() {
     let mut suite = ClearingQueueTestingSuiteBuilder::default()
-        .with_input_balances(vec![coin(1_000, DENOM_1), coin(1_000, DENOM_2)])
+        .with_input_balance(coin(1_000, DENOM_1))
         .build();
 
     let queue_len_0 = suite.query_queue_info().len;
@@ -132,13 +130,13 @@ fn test_queue_operates_in_fifo_manner() {
 
     //  user_2 -> user_3 -> user_1 -> head
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(100, DENOM_1), 1)
+        .register_new_obligation(suite.user_1.to_string(), 100u128.into(), 1)
         .unwrap();
     suite
-        .register_new_obligation(suite.user_3.to_string(), coins(200, DENOM_2), 2)
+        .register_new_obligation(suite.user_3.to_string(), 200u128.into(), 2)
         .unwrap();
     suite
-        .register_new_obligation(suite.user_2.to_string(), coins(420, DENOM_1), 3)
+        .register_new_obligation(suite.user_2.to_string(), 420u128.into(), 3)
         .unwrap();
 
     let queue_len = suite.query_queue_info().len;
@@ -172,94 +170,53 @@ fn test_queue_operates_in_fifo_manner() {
 }
 
 #[test]
-#[should_panic(expected = "obligation #10 is already registered in the queue")]
+#[should_panic(expected = "obligation being registered id out of order: expected 11, got 10")]
 fn test_double_accounting_errors() {
-    let mut suite = ClearingQueueTestingSuiteBuilder::default().build();
+    let mut suite = ClearingQueueTestingSuiteBuilder::default()
+        .with_latest_obligation_id(10)
+        .build();
 
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(100, DENOM_1), 10)
+        .register_new_obligation(suite.user_1.to_string(), 100u128.into(), 10)
         .unwrap();
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(200, DENOM_1), 10)
+        .register_new_obligation(suite.user_1.to_string(), 200u128.into(), 10)
         .unwrap();
 }
 
 #[test]
-#[should_panic(expected = "obligation #10 is already registered in the queue")]
+#[should_panic(expected = "obligation being registered id out of order: expected 11, got 10")]
 fn test_double_accounting_errors_after_obligation_settlement() {
     let mut suite = ClearingQueueTestingSuiteBuilder::default()
-        .with_input_balances(vec![coin(1_000, DENOM_1), coin(1_000, DENOM_2)])
+        .with_input_balance(coin(1_000, DENOM_1))
+        .with_latest_obligation_id(10)
         .build();
 
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(100, DENOM_1), 10)
+        .register_new_obligation(suite.user_1.to_string(), 100u128.into(), 10)
         .unwrap();
 
     suite.settle_next_obligation().unwrap();
 
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(200, DENOM_1), 10)
+        .register_new_obligation(suite.user_1.to_string(), 200u128.into(), 10)
         .unwrap();
-}
-
-#[test]
-fn test_user_obligation_with_multiple_denoms() {
-    let mut suite = ClearingQueueTestingSuiteBuilder::default()
-        .with_input_balances(vec![coin(100, DENOM_1), coin(100, DENOM_2)])
-        .build();
-
-    let input_acc_d1_bal = suite.query_input_acc_bal(DENOM_1);
-    let input_acc_d2_bal = suite.query_input_acc_bal(DENOM_2);
-    let u1_d1_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_1);
-    let u1_d2_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_2);
-
-    assert_eq!(input_acc_d1_bal.amount.u128(), 100);
-    assert_eq!(input_acc_d2_bal.amount.u128(), 100);
-    assert!(u1_d1_bal.amount.is_zero());
-    assert!(u1_d2_bal.amount.is_zero());
-
-    suite
-        .register_new_obligation(
-            suite.user_1.to_string(),
-            vec![coin(100, DENOM_1), coin(100, DENOM_2)],
-            1,
-        )
-        .unwrap();
-
-    let obligation_status = suite.query_obligation_status(1);
-    assert!(!obligation_status.settled);
-
-    suite.settle_next_obligation().unwrap();
-
-    let obligation_status = suite.query_obligation_status(1);
-    assert!(obligation_status.settled);
-
-    let input_acc_d1_bal = suite.query_input_acc_bal(DENOM_1);
-    let input_acc_d2_bal = suite.query_input_acc_bal(DENOM_2);
-    let u1_d1_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_1);
-    let u1_d2_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_2);
-
-    assert!(input_acc_d1_bal.amount.is_zero());
-    assert!(input_acc_d2_bal.amount.is_zero());
-
-    assert_eq!(u1_d1_bal.amount.u128(), 100);
-    assert_eq!(u1_d2_bal.amount.u128(), 100);
 }
 
 #[test]
 fn test_multi_user_settlement() {
     let mut suite = ClearingQueueTestingSuiteBuilder::default()
-        .with_input_balances(coins(1_000, DENOM_1))
+        .with_input_balance(coin(1_000, DENOM_1))
         .build();
 
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(100, DENOM_1), 1)
+        .register_new_obligation(suite.user_1.to_string(), 100u128.into(), 1)
         .unwrap();
     suite
-        .register_new_obligation(suite.user_2.to_string(), coins(300, DENOM_1), 2)
+        .register_new_obligation(suite.user_2.to_string(), 300u128.into(), 2)
         .unwrap();
     suite
-        .register_new_obligation(suite.user_3.to_string(), coins(400, DENOM_1), 3)
+        .register_new_obligation(suite.user_3.to_string(), 400u128.into(), 3)
         .unwrap();
 
     let input_acc_d1_bal = suite.query_input_acc_bal(DENOM_1);
@@ -272,9 +229,18 @@ fn test_multi_user_settlement() {
     assert!(u2_d1_bal.amount.is_zero());
     assert!(u3_d1_bal.amount.is_zero());
 
-    assert!(!suite.query_obligation_status(1).settled);
-    assert!(!suite.query_obligation_status(2).settled);
-    assert!(!suite.query_obligation_status(3).settled);
+    assert!(matches!(
+        suite.query_obligation_status(1),
+        ObligationStatus::InQueue
+    ));
+    assert!(matches!(
+        suite.query_obligation_status(2),
+        ObligationStatus::InQueue
+    ));
+    assert!(matches!(
+        suite.query_obligation_status(3),
+        ObligationStatus::InQueue
+    ));
 
     // settle all existing obligations
     suite.settle_next_obligation().unwrap();
@@ -287,9 +253,21 @@ fn test_multi_user_settlement() {
     let u3_d1_bal = suite.query_user_bal(suite.user_3.as_str(), DENOM_1);
 
     // assert that the obligations are now considered as settled
-    assert!(suite.query_obligation_status(1).settled);
-    assert!(suite.query_obligation_status(2).settled);
-    assert!(suite.query_obligation_status(3).settled);
+    let obligation_status_1 = suite.query_obligation_status(1);
+    println!("obligation status 1 : {:?} ", obligation_status_1);
+
+    assert!(matches!(
+        suite.query_obligation_status(1),
+        ObligationStatus::Processed
+    ));
+    assert!(matches!(
+        suite.query_obligation_status(2),
+        ObligationStatus::Processed
+    ));
+    assert!(matches!(
+        suite.query_obligation_status(3),
+        ObligationStatus::Processed
+    ));
 
     // assert that the settlement account balance is decreased as expected
     assert_eq!(input_acc_d1_bal.amount.u128(), 1_000 - 100 - 300 - 400);
@@ -300,41 +278,16 @@ fn test_multi_user_settlement() {
 }
 
 #[test]
-fn test_multi_denom_settlement() {
+#[should_panic(expected = "obligation being registered id out of order: expected 2, got 3")]
+fn test_registering_obligations_validates_order() {
     let mut suite = ClearingQueueTestingSuiteBuilder::default()
-        .with_input_balances(vec![coin(1_000, DENOM_1), coin(1_000, DENOM_2)])
+        .with_input_balance(coin(1_000, DENOM_1))
         .build();
 
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(100, DENOM_1), 1)
+        .register_new_obligation(suite.user_1.to_string(), 100u128.into(), 1)
         .unwrap();
     suite
-        .register_new_obligation(suite.user_1.to_string(), coins(300, DENOM_2), 2)
+        .register_new_obligation(suite.user_2.to_string(), 300u128.into(), 3)
         .unwrap();
-
-    let input_acc_d1_bal = suite.query_input_acc_bal(DENOM_1);
-    let input_acc_d2_bal = suite.query_input_acc_bal(DENOM_2);
-    let u1_d1_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_1);
-    let u1_d2_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_2);
-
-    assert_eq!(input_acc_d1_bal.amount.u128(), 1_000);
-    assert_eq!(input_acc_d2_bal.amount.u128(), 1_000);
-    assert!(u1_d1_bal.amount.is_zero());
-    assert!(u1_d2_bal.amount.is_zero());
-
-    // settle all existing obligations
-    suite.settle_next_obligation().unwrap();
-    suite.settle_next_obligation().unwrap();
-
-    let input_acc_d1_bal = suite.query_input_acc_bal(DENOM_1);
-    let input_acc_d2_bal = suite.query_input_acc_bal(DENOM_2);
-    let u1_d1_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_1);
-    let u1_d2_bal = suite.query_user_bal(suite.user_1.as_str(), DENOM_2);
-
-    assert_eq!(input_acc_d1_bal.amount.u128(), 1_000 - 100);
-    assert_eq!(input_acc_d2_bal.amount.u128(), 1_000 - 300);
-
-    // assert that user was credited as expected
-    assert_eq!(u1_d1_bal.amount.u128(), 100);
-    assert_eq!(u1_d2_bal.amount.u128(), 300);
 }
