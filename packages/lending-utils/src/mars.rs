1cb4fb8d3a312f918e31e1c35ae94161e5b8306d
bekauz <bekauz@protonmail.com>
2025-06-04 17:10:23 +0200

    add mars positions query

--- packages/lending-utils/src/mars.rs
+++ packages/lending-utils/src/mars.rs
@@ -4,6 +4,7 @@
 
 use cosmwasm_schema::cw_serde;
 use cosmwasm_schema::QueryResponses;
+use cosmwasm_std::Addr;
 use cosmwasm_std::{Coin, Uint128};
 
 #[cw_serde]
@@ -15,6 +16,9 @@ pub enum QueryMsg {
         start_after: Option<String>,
         limit: Option<u32>,
     },
+    /// All positions represented by token with value
+    #[returns(Positions)]
+    Positions { account_id: String },
 }
 
 #[cw_serde]
@@ -34,6 +38,66 @@ pub enum AccountKind {
     FundManager {
         vault_addr: String,
     },
+}
+
+#[cw_serde]
+pub struct Positions {
+    pub account_id: String,
+    pub account_kind: AccountKind,
+    pub deposits: Vec<Coin>,
+    pub debts: Vec<DebtAmount>,
+    pub lends: Vec<Coin>,
+    pub vaults: Vec<VaultPosition>,
+    pub staked_astro_lps: Vec<Coin>,
+}
+
+#[cw_serde]
+pub struct VaultPosition {
+    pub vault: Vault,
+    pub amount: VaultPositionAmount,
+}
+
+pub type Vault = VaultBase<Addr>;
+
+#[cw_serde]
+#[derive(Eq, Hash)]
+pub struct VaultBase<T> {
+    pub address: T,
+}
+
+#[cw_serde]
+pub enum VaultPositionAmount {
+    Unlocked(VaultAmount),
+    Locking(LockingVaultAmount),
+}
+
+#[cw_serde]
+pub struct LockingVaultAmount {
+    pub locked: VaultAmount,
+    pub unlocking: UnlockingPositions,
+}
+
+#[cw_serde]
+pub struct UnlockingPositions(Vec<VaultUnlockingPosition>);
+
+#[cw_serde]
+pub struct VaultAmount(Uint128);
+
+#[cw_serde]
+pub struct VaultUnlockingPosition {
+    /// Unique identifier representing the unlocking position. Needed for `ExecuteMsg::WithdrawUnlocked {}` call.
+    pub id: u64,
+    /// Coins that are awaiting to be unlocked (underlying, not vault tokens)
+    pub coin: Coin,
+}
+
+#[cw_serde]
+pub struct DebtAmount {
+    pub denom: String,
+    /// number of shares in debt pool
+    pub shares: Uint128,
+    /// amount of coins
+    pub amount: Uint128,
 }
 
 #[cw_serde]
