39d1c75ff4e251f0ce03b7326a28f684151d6874
Sam Hart <sam@hxrts.com>
2025-06-10 08:46:50 +0200

    Fix ExecuteMsg enum formatting - Format Execute, ApproveLibrary, and RemoveLibrary variants to single lines

--- contracts/accounts/jit_account/src/msg.rs
+++ contracts/accounts/jit_account/src/msg.rs
@@ -11,17 +11,11 @@ pub struct InstantiateMsg {
 #[cw_serde]
 pub enum ExecuteMsg {
     /// Execute a message on behalf of the account
-    Execute {
-        msgs: Vec<CosmosMsg>,
-    },
+    Execute { msgs: Vec<CosmosMsg> },
     /// Approve a library to execute messages
-    ApproveLibrary {
-        library: String,
-    },
+    ApproveLibrary { library: String },
     /// Remove approval for a library
-    RemoveLibrary {
-        library: String,
-    },
+    RemoveLibrary { library: String },
 }
 
 #[cw_serde]
