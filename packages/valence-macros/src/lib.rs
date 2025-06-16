3698eb2ce27b821882b2a303041d4366960a488c
keyleu <kleuyckx@gmail.com>
2025-06-16 18:53:57 +0100

    avoid doing it for option update

--- packages/valence-macros/src/lib.rs
+++ packages/valence-macros/src/lib.rs
@@ -33,7 +33,6 @@ pub fn valence_library_interface_derive(input: TokenStream) -> TokenStream {
 
         if let Some(inner_type) = get_option_inner_type(ty) {
             quote! {
-                #[serde(skip_serializing_if = "OptionUpdate::is_none")]
                 #vis #name: OptionUpdate<#inner_type>,
             }
         } else {
