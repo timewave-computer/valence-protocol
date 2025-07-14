d500ea7a7ace0cb4f905e2ff62623ac33ae11fb2
Keyne <kleuyckx@gmail.com>
2025-07-14 12:55:38 +0100

    audit: dont allow unpause if rate is stale (#414)

--- solidity/src/vaults/OneWayVault.sol
+++ solidity/src/vaults/OneWayVault.sol
@@ -515,6 +515,13 @@ contract OneWayVault is
         if (_vaultState.pausedByStaleRate && msg.sender != owner()) {
             revert("Only owner can unpause if paused by stale rate");
         }
+        if (
+            _vaultState.pausedByStaleRate
+                && uint64(block.timestamp) - lastRateUpdateTimestamp > config.maxRateUpdateDelay
+        ) {
+            revert("Cannot unpause while rate is stale");
+        }
+
         delete vaultState;
         emit PausedStateChanged(false);
     }
