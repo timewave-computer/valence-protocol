8530225ae9e3c6c791041b52fe7737c92024e1c0
bekauz <bekauz@protonmail.com>
2025-09-25 18:24:08 +0200

    set up testing

--- solidity/src/vaults/ValenceXCV.sol
+++ solidity/src/vaults/ValenceXCV.sol
@@ -2,50 +2,74 @@
 pragma solidity ^0.8.28;
 
 import {ERC4626Upgradeable} from "@openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import {BaseAccount} from "../accounts/BaseAccount.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {OwnableUpgradeable} from "@openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {ReentrancyGuardUpgradeable} from "@openzeppelin-contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import {UUPSUpgradeable} from "@openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract ValenceXCV is
-    Initializable,
-    ERC4626Upgradeable,
-    OwnableUpgradeable,
-    ReentrancyGuardUpgradeable,
-    UUPSUpgradeable
-{
+contract ValenceXCV is Initializable, ERC4626Upgradeable, OwnableUpgradeable {
+    // current share price
     uint256 public sharePrice;
 
+    // authorized strategist address
+    address public strategist;
+
+    // Valence account for holding the deposited funds
+    address public depositAccount;
+
+    error InvalidSharePrice();
+
+    error OnlyStrategistAllowed();
+
+    error DepositAccountNotSet();
+    error StrategistNotSet();
+
+    event SharePriceUpdated(uint256 indexed sharePrice);
+
+    modifier onlyStrategist() {
+        if (msg.sender != strategist) {
+            revert OnlyStrategistAllowed();
+        }
+        _;
+    }
+
     constructor() {
-        _disableInitializers();
+        // _disableInitializers();
     }
 
     function initialize(
         address _owner,
+        address strategistAddress,
         address underlying,
+        address depositAccountAddress,
         string memory vaultTokenName,
         string memory vaultTokenSymbol,
         uint256 startSharePrice
     ) external initializer {
+        // initialize the vault share token
         __ERC20_init(vaultTokenName, vaultTokenSymbol);
+        // initialize the underlying (deposit) token
         __ERC4626_init(IERC20(underlying));
+        // set up ownership
         __Ownable_init(_owner);
-        __ReentrancyGuard_init();
-        __UUPSUpgradeable_init();
 
+        if (startSharePrice == 0) revert InvalidSharePrice();
         sharePrice = startSharePrice;
+
+        if (depositAccountAddress == address(0)) revert DepositAccountNotSet();
+        depositAccount = depositAccountAddress;
+
+        if (strategistAddress == address(0)) revert StrategistNotSet();
+        strategist = strategistAddress;
     }
 
-    /**
-     * @dev Function that authorizes contract upgrades - required by UUPSUpgradeable
-     * @param newImplementation address of the new implementation
-     */
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal override onlyOwner {
-        // Upgrade logic comes here
-        // No additional logic required beyond owner check in modifier
+    function setSharePrice(uint256 newSharePrice) external onlyStrategist {
+        // setting share price to 0 would allow for infinite mint
+        if (newSharePrice == 0) revert InvalidSharePrice();
+
+        sharePrice = newSharePrice;
+
+        emit SharePriceUpdated(newSharePrice);
     }
 }
--- solidity/test/vaults/ValenceXCV.t.sol
+++ solidity/test/vaults/ValenceXCV.t.sol
@@ -1,17 +1,67 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.28;
+
 import {Test, console} from "forge-std/src/Test.sol";
 import {ValenceXCV} from "../../src/vaults/ValenceXCV.sol";
+import {BaseAccount} from "../../src/accounts/BaseAccount.sol";
+import {MockERC20} from "../mocks/MockERC20.sol";
 
 // run with: forge test --match-path test/vaults/ValenceXCV.t.sol -vvv
 
 contract ValenceXCVTest is Test {
+    // contracts
     ValenceXCV internal vault;
+    MockERC20 internal underlyingToken;
+    BaseAccount internal depositAccount;
+
+    // test addresses
+    address owner = address(1);
+    address strategist = address(2);
+    address user1 = address(3);
+    address user2 = address(4);
+
+    // vault config
+    uint256 initialSharePrice = 10 ** 18; // 1:1 initial rate
 
     function setUp() public {
-        console.log("Setting up ValenceXCVTest");
+        vm.startPrank(owner);
+
+        // deploy mock token and deposit account
+        underlyingToken = new MockERC20("Test Token", "TST", 18);
+        depositAccount = new BaseAccount(owner, new address[](0));
+
         vault = new ValenceXCV();
+
+        // initialize the vault
+        vault.initialize(
+            owner,
+            strategist,
+            address(underlyingToken),
+            address(depositAccount),
+            "ValenceXCV",
+            "vXCV",
+            initialSharePrice
+        );
+
+        underlyingToken.mint(user1, 1000 * 10 ** 18);
+        underlyingToken.mint(user2, 1000 * 10 ** 18);
+
+        vm.stopPrank();
+
+        // approve deposit tokens to be spent by the vault for users
+        vm.prank(user1);
+        underlyingToken.approve(address(vault), type(uint256).max);
+
+        vm.prank(user2);
+        underlyingToken.approve(address(vault), type(uint256).max);
     }
 
-    function testSetUpVault() public {
-        console.log("set up complete");
+    function testSetUpVault() public view {
+        assertEq(vault.owner(), owner);
+        assertEq(vault.strategist(), strategist);
+        assertEq(vault.depositAccount(), address(depositAccount));
+        assertEq(vault.name(), "ValenceXCV");
+        assertEq(vault.symbol(), "vXCV");
+        assertEq(vault.sharePrice(), initialSharePrice);
     }
 }
